c637a798948e42eed7de1732b3e364ca8bc678fe
nabuhandbz <90187970+nabuhandbz@users.noreply.github.com>
2021-09-15 15:34:55 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -21,7 +21,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Ruby bot by nabuhandbz*\n
 \n*owner number wa.me/918289836232*\n
 \n*CONTACT OWNER THROUGH social networks-https://www.instagram.com/im.naabu?r=nametag*\n
-\n*bot setting video : https://youtu.be/dSGrhJZtu24*\n
+
 \n*githublink       https://github.com/nabuhandbz/ruby*\n
 `}) 
 
