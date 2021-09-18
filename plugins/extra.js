7ef5e64eb46065e58452c703db313f2c588df412
nabuhandbz <90187970+nabuhandbz@users.noreply.github.com>
2021-09-19 01:04:01 +0530

    Update extra.js

--- plugins/extra.js
+++ plugins/extra.js
@@ -309,8 +309,8 @@ else if (config.WORKTYPE == 'public') {
             return;
         }
 
-        if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'ᴘɪɴᴋʏ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴀꜰɴᴀɴᴘʟᴋ' , MessageType.text, { quoted: message.data });
+        if (config.OWNER == 'default') {
+            await message.client.sendMessage(message.jid,'RUBY BOT CREATED BY NABUHANDBZ' , MessageType.text, { quoted: message.data });
         }
         else {
             await message.client.sendMessage(message.jid,config.PLK, MessageType.text, { quoted: message.data });
